72a0638176eef07d15066a0e6de9dd7e57b1cc98
Jackson W <jack@mail.tdstoragebay.com>
2021-03-21 03:56:33 -0400

    Get album name using Azuracast's NowPlaying API
    This method is... more than a little hacky, it pulls the /api/nowplaying/
    endpoint, finds the station data based on name, and then pulls album
    data from there. But for some reason, the XMLHttpRequest is always
    sending an HTTP request, even when manually specifying a full URL,
    'https://' included. So, you'll get one mixed content warning per
    second, but it will somehow get at least one that goes through and
    pulls the album name as expected.
    
    So, it works, but I have no idea why, and nothing I can control seems
    to be able to fix it.

--- src/connectors/azuracast.js
+++ src/connectors/azuracast.js
@@ -18,3 +18,31 @@ Connector.isTrackArtDefault = (url) => {
 Connector.currentTimeSelector = '.now-playing-details .now-playing-main .time-display .time-display-played.text-secondary';
 
 Connector.DurationSelector = '.now-playing-details .now-playing-main .time-display .time-display-total.text-secondary';
+
+Connector.getAlbum = () => {
+	const stationNameSelector = 'h2.card-title';
+	const stationName = Util.getTextFromSelectors(stationNameSelector);
+	let currentStation;
+	let albumName;
+
+	const xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+			const nowPlaying = JSON.parse(xmlHttp.responseText);
+			for (const station in nowPlaying) {
+				if (nowPlaying[station].station.name === stationName) {
+					currentStation = nowPlaying[station];
+				}
+			}
+			if (currentStation === null) {
+				albumName = null;
+			}
+
+			albumName = currentStation.now_playing.song.album;
+		}
+	};
+	xmlHttp.open('GET', '/api/nowplaying/', true);
+	xmlHttp.send(null);
+
+	return albumName;
+};
